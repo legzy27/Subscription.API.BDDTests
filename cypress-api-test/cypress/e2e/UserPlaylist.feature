3c3140e082174582988891593435d58586be9df8
Alex Oyedele <alexoyedele@Alexs-MacBook-Pro.local>
2023-12-06 08:24:56 +0000

    updated test and user stories files

--- cypress-api-test/cypress/e2e/UserPlaylist.feature
+++ cypress-api-test/cypress/e2e/UserPlaylist.feature
@@ -3,14 +3,14 @@ Feature: Free User Access to Playlists, Check Premium User Playlist Access And V
 
   Scenario: Free user requests playlists
     When I make an API request as a free user
-    Then I should receive a "200" response
+    Then I should receive a 200 OK response
 
   Scenario: Premium user accesses playlists
     When I request my playlists
-    Then I should receive a "200" response back
+    Then I should receive a 200 OK response
 
 
   Scenario: Request play lists with an unknown user type
     When I request playlists for this user type
-    Then I should receive a HTTP "400" Bad Request response
+    Then I should receive a HTTP 400 Bad Request response
     And the response message should indicate an "Unknown user type"
--- cypress-api-test/cypress/e2e/UserPlaylist/UserPlaylist.js
+++ cypress-api-test/cypress/e2e/UserPlaylist/UserPlaylist.js
@@ -1,47 +1,45 @@
-import {When, Then } from 'cypress-cucumber-preprocessor/steps';
-const { BASE_URL } = require('../../../config');
+import { When, Then } from "cypress-cucumber-preprocessor/steps";
+const { BASE_URL } = require("../../../config");
 
 let response;
 
 //Free User Access Test
-When('I make an API request as a free user', () => {
-    cy.request(`${BASE_URL}/free`)
-      .then((res) => {
-          response = res;
-      });
+When("I make an API request as a free user", () => {
+  cy.request(`${BASE_URL}/free`).then((res) => {
+    response = res;
+  });
 });
 
-Then('I should receive a {string} response', (statusCode) => {
-    expect(response.status).to.eq(parseInt(statusCode));
+Then("I should receive a {int} OK response", (statusCode) => {
+  expect(response.status).to.eq(parseInt(statusCode));
 });
 
 //Premium User Access Test
-When('I request my playlists', () => {
-    cy.request(`${BASE_URL}/premium`)
-      .then((res) => {
-          response = res;
-      });
+When("I request my playlists", () => {
+  cy.request(`${BASE_URL}/premium`).then((res) => {
+    response = res;
+  });
 });
 
-Then('I should receive a {string} response back', (statusCode) => {
-    expect(response.status).to.eq(parseInt(statusCode));
+Then("I should receive a {int} OK response", (statusCode) => {
+  expect(response.status).to.eq(parseInt(statusCode));
 });
 
 //Unknown User Access Playlist
-When('I request playlists for this user type', () => {
-    cy.request({
-        method: 'GET',
-        url: `${BASE_URL}/plus`,
-        failOnStatusCode: false
-    }).then((res) => {
-        response = res;
-    });
+When("I request playlists for this user type", () => {
+  cy.request({
+    method: "GET",
+    url: `${BASE_URL}/plus`,
+    failOnStatusCode: false,
+  }).then((res) => {
+    response = res;
+  });
 });
 
-Then('I should receive a HTTP {string} Bad Request response', () => {
-    expect(response.status).to.eq(400);
+Then("I should receive a HTTP {int} Bad Request response", () => {
+  expect(response.status).to.eq(400);
 });
 
 Then('the response message should indicate an "Unknown user type"', () => {
-    expect(response.body.messsage).to.eq('Unknown user type');
+  expect(response.body.messsage).to.eq("Unknown user type");
 });
